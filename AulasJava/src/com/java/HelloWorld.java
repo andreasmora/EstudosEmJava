efdf42068685e516ff5b2d074f6fb706e1dd75de
andremoraes <andrealvesmoraes75@gmail.com>
2024-01-02 00:06:37 -0300

    Estudos em Java

--- AulasJava/src/com/java/HelloWorld.java
+++ AulasJava/src/com/java/HelloWorld.java
@@ -2,7 +2,7 @@ package com.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World");
 
     }
 }
